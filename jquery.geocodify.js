5256077f6ddad7b6d82a249893b1b569664dcf4f
palewire <ben.welsh@gmail.com>
2011-08-04 10:34:31 -0700

    Tweaked prepSearch name to prepSearchString on advice from @schwanksta

--- jquery.geocodify.js
+++ jquery.geocodify.js
@@ -8,7 +8,7 @@
             regionBias = opts.regionBias || null,
             viewportBias = opts.viewportBias || null,
             onSelect  = opts.onSelect || function(ele) { alert('Jump to: ' + ele.formatted_address )},
-            prepSearch = opts.prepSearch || null,
+            prepSearchString = opts.prepSearchString || null,
             acceptableAddressTypes = opts.acceptableAddressTypes || [
                 'street_address',
                 'route',
@@ -63,8 +63,8 @@
                     $("#" + id + "-input").css("border", "1px solid #9C9C9C");
                     return false;
                 }
-                if (prepSearch) {
-                    query = prepSearch(query);
+                if (prepSearchString) {
+                    query = prepSearchString(query);
                 }
                 var params = { 'address': query };
                 if (regionBias) {
