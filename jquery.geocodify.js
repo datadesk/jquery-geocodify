b8546081b34843542a7538e2149bee9b3d3031f7
palewire <ben.welsh@gmail.com>
2011-08-05 08:21:10 -0700

    Refactored the library to do a more jQuer-ish extend on the default settings. Fixes #26.

--- jquery.geocodify.js
+++ jquery.geocodify.js
@@ -1,16 +1,17 @@
 (function( $ ){
     $.fn.geocodify = function(options) {
-        var opts = options || {},
-            width = opts.width || 400,
-            height = opts.height ||32,
-            fontSize = opts.fontSize || "16px",
-            buttonValue = opts.buttonValue || "GO",
-            regionBias = opts.regionBias || null,
-            viewportBias = opts.viewportBias || null,
-            onSelect  = opts.onSelect || function(ele) { alert('Jump to: ' + ele.formatted_address )},
-            prepSearchString = opts.prepSearchString || null,
-            initialText = opts.initialText || null,
-            acceptableAddressTypes = opts.acceptableAddressTypes || [
+
+        var settings = {
+          'width' : 400,
+          'height' : 32,
+          'fontSize': '16px',
+          'buttonValue': 'GO',
+          'regionBias': null,
+          'viewportBias': null,
+          'onSelect': function(ele) { alert('Jump to: ' + ele.formatted_address )},
+          'prepSearchString': null,
+          'initialText': null,
+          'acceptableAddressTypes': [
                 'street_address',
                 'route',
                 'intersection',
@@ -34,8 +35,8 @@
                 'street_number',
                 'floor',
                 'room'
-            ],
-            keyCodes = {
+            ], 
+            'keyCodes' : {
                 UP: 38,
                 DOWN: 40,
                 DEL: 46,
@@ -46,130 +47,135 @@
                 PAGEUP: 33,
                 PAGEDOWN: 34,
                 BACKSPACE: 8
-            };
-        
-        var Geocode = function(id, callback, regionBias, viewportBias) {
-            this.previousSearch = null;
-            this.google = new google.maps.Geocoder();
-            this.fetch = function(query, force) {
-                if (query === this.previousSearch && !(force)) {
-                    return false;
-                };
-                if (query === initialText) {
-                    return false;
-                };
-                this.previousSearch = query;
-                var qLength = query.length;
-                if (qLength < 5 && !(force)) {
-                    $("#" + id + "-dropdown").html("");
-                    $("#" + id + "-dropdown").hide();
-                    $("#" + id + "-close").hide();
-                    $("#" + id + "-input").css("border", "1px solid #9C9C9C");
-                    return false;
-                }
-                if (prepSearchString) {
-                    query = prepSearchString(query);
-                }
-                var params = { 'address': query };
-                if (regionBias) {
-                    params['region'] = regionBias;
-                };
-                if (viewportBias) {
-                    params['bounds'] = viewportBias;
-                };
-                this.google.geocode(params, onGeocode(id, force));
-            };
-        };
-        
-        var onGeocode = function(id, force) {
-            return function(results, status) {
-                // Line up all the object we'll be playing with
-                var dropdown = $("#" + id + "-dropdown"),
-                    input = $("#" + id + "-input"),
-                    close = $("#" + id + "-close");
-                
-                // Define what will happen when the form is reset
-                var reset = function () {
-                    dropdown.empty();
-                    dropdown.hide();
-                    close.hide();
-                    input.css({
-                        "border": "1px solid #9C9C9C"
-                    });
-                };
-                reset();
-                
-                // Loop through the results and filter out precision
-                // levels we will not accept.
-                var keep = new Array();
-                $.each(results, function(i, val) {
-                    $.each(val.types, function(ii, type) {
-                        if (new RegExp(type).test(acceptableAddressTypes.join("|"))) {
-                            keep.push(val);
-                            return false;
-                        }
-                    });
-                });
-                
-                var count = keep.length;
-                if (count === 0) {
-                    var ul = $("<ul>").css({'margin': 0, 'padding': 0, 'background-color': 'white'});
-                    var li = $("<li>")
-                        .html("No results found. Please refine your search.")
-                        .css({
-                                'cursor': 'pointer',
-                                'margin-left': 0,
-                                'padding': '5px 0 5px 8px',
-                                'list-style-type': 'none',
-                                'text-align': 'left'
-                            })
-                        .appendTo(ul)
-                    ul.appendTo(dropdown);
-                    dropdown.show();
-                    $("li", dropdown).css("cursor", "default");
-                    close.show();
-                    close.click(reset)
-                } else if (count === 1 && force) {
-                    onSelect(results[0]);
-                    reset();
-                } else {
-                    var ul = $("<ul>").css({'margin': 0, 'padding': 0, 'background-color': 'white'});
-                    $.each(keep, function(i, val) {
-                        $('<li>')
-                            .html(val.formatted_address)
-                            .css({
-                                'cursor': 'pointer',
-                                'margin-left': 0,
-                                'padding': '5px 0 5px 8px',
-                                'list-style-type': 'none',
-                                'font-size': fontSize,
-                                'text-align': 'left'
-                            })
-                            .click(function(){onSelect(val); reset();})
-                            .hover(
-                                function() { 
-                                    $(this).css({'background-color': '#EEE', 'cursor': 'pointer'});
-                                },
-                                function() {
-                                    $(this).css({'background-color': 'white', 'cursor': 'auto'});
-                                })
-                            .appendTo(ul);
-                    });
-                    ul.appendTo(dropdown);
-                    input.css({
-                        "border-top": "1px solid #2662CC",
-                        "border-right": "1px solid #2662CC",
-                        "border-left": "1px solid #2662CC"
-                    });
-                    dropdown.show();
-                    close.show();
-                    close.click(reset)
-                }
             }
-        }
-        
+        };
+                
         return this.each(function() {
             var $this = $(this);
+            
+            if ( options ) { 
+                $.extend( settings, options );
+            }
+            
+            var Geocode = function(id, callback, regionBias, viewportBias) {
+                this.previousSearch = null;
+                this.google = new google.maps.Geocoder();
+                this.fetch = function(query, force) {
+                    if (query === this.previousSearch && !(force)) {
+                        return false;
+                    };
+                    if (query === settings.initialText) {
+                        return false;
+                    };
+                    this.previousSearch = query;
+                    var qLength = query.length;
+                    if (qLength < 5 && !(force)) {
+                        $("#" + id + "-dropdown").html("");
+                        $("#" + id + "-dropdown").hide();
+                        $("#" + id + "-close").hide();
+                        $("#" + id + "-input").css("border", "1px solid #9C9C9C");
+                        return false;
+                    }
+                    if (settings.prepSearchString) {
+                        query = settings.prepSearchString(query);
+                    }
+                    var params = { 'address': query };
+                    if (regionBias) {
+                        params['region'] = regionBias;
+                    };
+                    if (viewportBias) {
+                        params['bounds'] = viewportBias;
+                    };
+                    this.google.geocode(params, onGeocode(id, force));
+                };
+            };
+            
+            var onGeocode = function(id, force) {
+                return function(results, status) {
+                    // Line up all the object we'll be playing with
+                    var dropdown = $("#" + id + "-dropdown"),
+                        input = $("#" + id + "-input"),
+                        close = $("#" + id + "-close");
+                    
+                    // Define what will happen when the form is reset
+                    var reset = function () {
+                        dropdown.empty();
+                        dropdown.hide();
+                        close.hide();
+                        input.css({
+                            "border": "1px solid #9C9C9C"
+                        });
+                    };
+                    reset();
+                    
+                    // Loop through the results and filter out precision
+                    // levels we will not accept.
+                    var keep = new Array();
+                    $.each(results, function(i, val) {
+                        $.each(val.types, function(ii, type) {
+                            if (new RegExp(type).test(settings.acceptableAddressTypes.join("|"))) {
+                                keep.push(val);
+                                return false;
+                            }
+                        });
+                    });
+                    
+                    var count = keep.length;
+                    if (count === 0) {
+                        var ul = $("<ul>").css({'margin': 0, 'padding': 0, 'background-color': 'white'});
+                        var li = $("<li>")
+                            .html("No results found. Please refine your search.")
+                            .css({
+                                    'cursor': 'pointer',
+                                    'margin-left': 0,
+                                    'padding': '5px 0 5px 8px',
+                                    'list-style-type': 'none',
+                                    'text-align': 'left'
+                                })
+                            .appendTo(ul)
+                        ul.appendTo(dropdown);
+                        dropdown.show();
+                        $("li", dropdown).css("cursor", "default");
+                        close.show();
+                        close.click(reset)
+                    } else if (count === 1 && force) {
+                        settings.onSelect(results[0]);
+                        reset();
+                    } else {
+                        var ul = $("<ul>").css({'margin': 0, 'padding': 0, 'background-color': 'white'});
+                        $.each(keep, function(i, val) {
+                            $('<li>')
+                                .html(val.formatted_address)
+                                .css({
+                                    'cursor': 'pointer',
+                                    'margin-left': 0,
+                                    'padding': '5px 0 5px 8px',
+                                    'list-style-type': 'none',
+                                    'font-size': settings.fontSize,
+                                    'text-align': 'left'
+                                })
+                                .click(function(){settings.onSelect(val); reset();})
+                                .hover(
+                                    function() { 
+                                        $(this).css({'background-color': '#EEE', 'cursor': 'pointer'});
+                                    },
+                                    function() {
+                                        $(this).css({'background-color': 'white', 'cursor': 'auto'});
+                                    })
+                                .appendTo(ul);
+                        });
+                        ul.appendTo(dropdown);
+                        input.css({
+                            "border-top": "1px solid #2662CC",
+                            "border-right": "1px solid #2662CC",
+                            "border-left": "1px solid #2662CC"
+                        });
+                        dropdown.show();
+                        close.show();
+                        close.click(reset)
+                    }
+                }
+            }
             
             // Clear out any existing stuff inside the form and set its style
             $this
@@ -178,7 +184,7 @@
                     'position': 'relative',
                     'margin': 0,
                     'padding': 0,
-                    'width': width,
+                    'width': settings.width,
                     'z-index': 9001
                 });
             document.getElementById($this.attr("id")).setAttribute("autocomplete", "off");
@@ -193,19 +199,19 @@
                     'position': 'absolute',
                     'top': 0,
                     'left': 0,
-                    'width': width,
-                    'height': height - 1,
-                    'line-height': fontSize,
-                    'font-size': fontSize
+                    'width': settings.width,
+                    'height': settings.height - 1,
+                    'line-height': settings.fontSize,
+                    'font-size': settings.fontSize
                 })
                 .addClass("geocodifyInput")
                 .appendTo($this);
             document.getElementById(inputId).setAttribute("autocomplete", "off");
             var input = $("#" + inputId);
-            if (initialText) {
-                input.val(initialText);
+            if (settings.initialText) {
+                input.val(settings.initialText);
                 input.focus(function() {
-                    if (input.val() == initialText) {
+                    if (input.val() == settings.initialText) {
                         input.val("");
                     }
                 });
@@ -215,14 +221,14 @@
             var buttonId = $this.attr("id") + "-button";
             $('<input>')
                 .attr({type: 'button', id: buttonId,
-                    value: buttonValue})
+                    value: settings.buttonValue})
                 .css({
                     'position': 'absolute',
                     'top': 0,
-                    'left': width + 10,
+                    'left': settings.width + 10,
                     'padding': '4px',
                     'margin': 0,
-                    'font-size': fontSize
+                    'font-size': settings.fontSize
                 })
                 .addClass("geocodifyButton")
                 .appendTo($this);
@@ -255,7 +261,7 @@
                     'top': input.height() + 6,
                     'left': 0,
                     'border': '1px solid #CCC',
-                    'width': width - 2,
+                    'width': settings.width - 2,
                     'z-index': 8001
                 })
                 .addClass("geocodifyDropdown")
@@ -264,7 +270,7 @@
             var dropdown = $("#" + dropdownId);
             
             // Bind our geocoding operation to the form
-            var app = new Geocode($this.attr("id"), onGeocode, regionBias, viewportBias);
+            var app = new Geocode($this.attr("id"), onGeocode, settings.regionBias, settings.viewportBias);
             setInterval(function(){app.fetch(input.val(), false)}, 250);
             $this.submit(function(){return false;});
             button.click(function(){app.fetch(input.val(), true);return false;});
@@ -272,7 +278,7 @@
             // Bind key up and down events
             $this.bind(($.browser.opera ? "keypress" : "keydown"), function(event) {
                 switch(event.keyCode) {
-                    case keyCodes.UP:
+                    case settings.keyCodes.UP:
                         var resultList = $("li", dropdown);
                         var selectedIndex = 0;
                         $.each(resultList, function(i, li) {
@@ -290,7 +296,7 @@
                             .addClass("selected")
                             .css({'background-color': '#EEE'});
                         break;
-                    case keyCodes.DOWN:
+                    case settings.keyCodes.DOWN:
                         var resultList = $("li", dropdown);
                         var selectedIndex = -1;
                         $.each(resultList, function(i, li) {
@@ -307,7 +313,7 @@
                             .addClass("selected")
                             .css({'background-color': '#EEE'});
                         break;
-                    case keyCodes.RETURN:
+                    case settings.keyCodes.RETURN:
                         var resultList = $("li.selected", dropdown);
                         if (resultList) {
                             resultList.click();
